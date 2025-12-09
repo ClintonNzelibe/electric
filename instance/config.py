4d61dd376576b0d0049d7e51cc995db3f4678eed
“kayc” <kayctush@gmail.com>
2025-12-09 06:36:42 -0800

    Update database config and starter for Railway deployment

--- instance/config.py
+++ instance/config.py
@@ -4,6 +4,15 @@ SECRET_KEY = 'COuT9Mlb1oLA5Q'
 SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root@localhost/powerhouse'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = int(os.environ.get("DB_PORT", 3306))
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
 UPLOAD_FOLDER = 'elec/static/uploads/'
--- starter.py
+++ starter.py
@@ -1,7 +1,9 @@
-
-from elec import create_app
+from elec import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()  # <-- this ensures tables are created when the app starts
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
