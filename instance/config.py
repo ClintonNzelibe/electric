8eb7130a9e248c4cee26eb2973051accec64adcf
“kayc” <kayctush@gmail.com>
2025-12-09 07:21:34 -0800

    Fix DB_HOST and DB_NAME defaults for Railway deployment

--- instance/config.py
+++ instance/config.py
@@ -2,12 +2,12 @@ import os
 
 SECRET_KEY = 'COuT9Mlb1oLA5Q'
 
-
-DB_HOST = os.environ.get("DB_HOST")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = int(os.environ.get("DB_PORT", 3306))
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER", "root")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+# Use Railway DB name if available, else local
+DB_NAME = os.environ.get("MYSQLDATABASE", "powerhouse")
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
